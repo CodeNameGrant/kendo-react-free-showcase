3a428ad7dc17aa717e8577fd6be73e3a53d38814
Grant Walker <GrantW2505@gmail.com>
2025-03-17 23:13:48 +0200

    Fixed tsx smell

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 export default function Section({ children }: { children: ReactNode }) {
   return <div className="border border-black rounded p-2">{children}</div>;
